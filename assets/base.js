0799eab0b552ba559b8349cff44cd97417f169fe
aznoe <39156100+aznoe@users.noreply.github.com>
2025-04-02 10:10:01 +0000

    started 9:30 ended 11:40

--- assets/base.js
+++ assets/base.js
@@ -47,4 +47,61 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Call the function on scroll
     window.addEventListener('scroll', highlightCurrentSection);
+
+
+    // CTA button on the banner
+    const cta = document.querySelector('.banner-btn');
+    const popup = document.querySelector('.popup-modal');
+
+    // open when clicking the CTA
+    cta.addEventListener('click', () => {
+        document.body.style.overflow = 'hidden';
+        popup.style.display = 'block';    
+        setTimeout(() => {
+            popup.style.opacity = '100%';
+        },10);
+    });
+
+    // close when clicking outside the popup content
+    window.addEventListener('click', (e) => {
+        if (e.target === popup) {
+            document.body.style.overflow = '';
+            popup.style.opacity = '0%';
+            setTimeout(() => {
+                popup.style.display = 'none';   
+            }, 300);
+        }
+    });
+
+    // Countdown timer:
+    function shipmentCountdownTimer(){
+        let now = new Date();
+        let deadline = new Date();
+        deadline.setHours(15, 0, 0, 0); // kl 15:00
+
+        let countDownTimer = document.getElementById('countdown-timer');
+        let deadlineText = document.getElementById('deadline-text');
+        
+        // difference between deadline and the time now
+        let difference = deadline - now;
+
+        // countdown calculations
+        let hours = Math.floor(difference / (1000 * 60 * 60));
+        let min = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        let sec = Math.floor((difference % (1000 * 60)) / 1000);
+
+        if(now < deadline){
+            // set the time into the html
+            countDownTimer.textContent = `${hours} : ${min} : ${sec}`
+
+        } else{
+            // when time excedes the deadline
+            deadlineText.textContent = "Bestil inden kl. 15 for afsendelse samme dag.";
+            countDownTimer.textContent = ``;
+        }
+
+    }
+
+    shipmentCountdownTimer();
+    setInterval(shipmentCountdownTimer, 1000); 
 });
